b916b3d733b295298c69d12c6796c78f7fc2975d
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-06 11:19:01 +0900

    [🎨스타일] Signin, Signup 페이지 컴포넌트에 CSS파일 import

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,3 +1,5 @@
+import styles from "./Signin.module.css";
+
 export function Signin() {
   return(
     <div>
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,3 +1,5 @@
+import styles from "./Signup.module.css";
+
 export function Signup() {
   return(
     <div>
