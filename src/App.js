aa2a1fffe0b973af4cfa41bcfbaedcc23a6034f2
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-06 13:50:27 +0900

    [✨기능] 리액트 라우터로 Signup 페이지 컴포넌트 경로 임시 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Signup } from "@/pages/index";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
