d98e974dd6d6f02a4739f4fe481786ad5cd87587
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-06 13:50:58 +0900

    [🎨스타일] Signup 페이지 컴포넌트 생성

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,9 +1,15 @@
+import { Form, Input, Button, Heading1 } from '@/components';
 import styles from "./Signup.module.css";
 
 export function Signup() {
   return(
-    <div>
-      <h1>회원가입</h1>
+    <div className={styles.signup}>
+      <Heading1 className={styles.h1}>회원가입</Heading1>
+      <Form legend="회원가입" className={styles.form}>
+        <Input className={styles.input} name="email" label="이메일" type="text" placeholder='이메일'></Input>
+        <Input className={styles.input} name="password" label="비밀번호" type="password" placeholder='비밀번호'></Input>
+        <Button className={styles.button}>회원가입</Button>
+      </Form>
     </div>
   )
 }
