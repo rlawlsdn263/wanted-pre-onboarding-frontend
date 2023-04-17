e630e80e1d5ca4b4d58d77d508235525f8bd62a1
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-17 16:52:13 +0900

    [✨기능] Link로 네비게이션바 제작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Signup, Signin, Todo } from "@/pages/index";
 
 function App() {
   return (
     <div className="App">
+      <ul className="navbar">
+        <li>
+          <Link to="/signup">회원가입</Link>
+        </li>
+        <li>
+          <Link to="/signin">로그인</Link>
+        </li>
+        <li>
+          <Link to="/todo">투두리스트</Link>
+        </li>
+      </ul>
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/todo" element={<Todo />} />
       </Routes>
