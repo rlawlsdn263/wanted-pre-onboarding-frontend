3411dfeaee3925ec6cc371e0b2bbd60b22b8f486
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-17 14:30:28 +0900

    [🎨스타일] Signin 페이지 컴포넌트 스타일 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { Signup } from "@/pages/index";
+import { Signup, Signin } from "@/pages/index";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
       </Routes>
     </div>
   );
--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,9 +1,15 @@
 import styles from "./Signin.module.css";
+import { Form, Input, Button, Heading1 } from '@/components';
 
 export function Signin() {
   return(
-    <div>
-      <h1>로그인</h1>
+    <div className={styles.signin}>
+      <Heading1 className={styles.h1}>로그인</Heading1>
+      <Form legend="로그인" className={styles.form}>
+        <Input data-testid="email-input" className={styles.input} name="email" label="이메일" type="text" placeholder='test@test.com'></Input>
+        <Input data-testid="password-input" className={styles.input} name="password" label="비밀번호" type="password" placeholder='12345678'></Input>
+        <Button data-testid="signup-button" className={styles.button}>로그인</Button>
+      </Form>
     </div>
   )
 }
