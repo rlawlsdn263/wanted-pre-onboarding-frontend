41e0f50267c0eea2ee6baec1310d02cab092fa78
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-17 15:58:50 +0900

    [✨기능] Signin 페이지 컴포넌트 로그인 기능 완성
    - useRef로 input값에 접근함
    - checkUserWithEmailAndPassword()에서 POST 통신을 보내 input값과 매치하는 값이 있는 지 확인
    - 로그인에 성공할 경우 localStorage에 JWT 토큰을 저장하고 /todo로 페이지 리디렉션을 함

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,14 +1,52 @@
+import { useRef } from 'react';
 import styles from "./Signin.module.css";
 import { Form, Input, Button, Heading1 } from '@/components';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export function Signin() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const navigate = useNavigate();
+
+  //유저정보를 확인해주는 함수
+  function checkUserWithEmailAndPassword(email, password) {
+    axios.post('https://www.pre-onboarding-selection-task.shop/auth/signin', {
+      email: email,
+      password: password,
+    }, {headers: {
+      "Content-Type": "application/json",
+    }})
+    .then(response => {
+      console.log(response);
+      // JWT 토큰을 localStorage에 저장함
+      localStorage.setItem("user", JSON.stringify(response.data));
+      alert("로그인 성공!");
+      //로그인 성공할 경우 todo로 페이지 리디렉션을 해줌
+      navigate('/todo');
+    })
+    .catch(error => {
+      console.log(error);
+      //로그인 실패할 경우 input 값을 비어줌
+      emailRef.current.value = "";
+      passwordRef.current.value = "";
+      alert("로그인 실패!")
+    });
+  }
+
+  function handleClick() {
+    // 이메일과 비밀번호로 사용자 정보가 있는 지 확인해줌
+    checkUserWithEmailAndPassword(emailRef.current.value, passwordRef.current.value);
+  }
+
   return(
     <div className={styles.signin}>
       <Heading1 className={styles.h1}>로그인</Heading1>
       <Form legend="로그인" className={styles.form}>
-        <Input data-testid="email-input" className={styles.input} name="email" label="이메일" type="text" placeholder='test@test.com'></Input>
-        <Input data-testid="password-input" className={styles.input} name="password" label="비밀번호" type="password" placeholder='12345678'></Input>
-        <Button data-testid="signup-button" className={styles.button}>로그인</Button>
+        <Input data-testid="email-input" forwardRef={emailRef} className={styles.input} name="email" label="이메일" type="text" placeholder='test@test.com'></Input>
+        <Input data-testid="password-input" forwardRef={passwordRef} className={styles.input} name="password" label="비밀번호" type="password" placeholder='12345678'></Input>
+        <Button data-testid="signup-button" onClick={handleClick} className={styles.button}>로그인</Button>
       </Form>
     </div>
   )
