f6dc429c74effa7022bd2ae0557031a9ef1c3de5
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-17 10:50:49 +0900

    [✨기능] Signup 페이지 회원가입 기능 구현
    - useRef를 사용해 재렌더링 없이 input에 입력된 값을 불러옴
    - 회원가입 버튼을 누를 경우 유효성 검증 함수가 실행되고 성공 혹은 실패 여부를 알려줌

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,10 @@
 import styles from "./Input.module.css";
 
-export function Input({name, label, type, ...rest}) {
+export function Input({name, label, type, forwardRef, ...rest}) {
   return(
     <div>
       <label htmlFor={name} className={styles.a11yHidden}>{label}</label>
-      <input id={name} name={name} type={type} {...rest}/>
+      <input id={name} name={name} type={type} ref={forwardRef} {...rest}/>
     </div>
   )
 }
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,14 +1,44 @@
 import { Form, Input, Button, Heading1 } from '@/components';
 import styles from "./Signup.module.css";
+import { useRef } from 'react';
 
 export function Signup() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  function checkEmailInput() {
+    const regex = /@/;
+    if (!regex.test(emailRef.current.value)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function checkPasswordInput() {
+    const regex = /^.{8,}$/;
+    if (!regex.test(passwordRef.current.value)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function handleClickToRegister() {
+    if(checkEmailInput() && checkPasswordInput()){
+      alert("회원가입 성공");
+    } else {
+      alert("회원가입 실패")
+    }
+  }
+
   return(
     <div className={styles.signup}>
       <Heading1 className={styles.h1}>회원가입</Heading1>
       <Form legend="회원가입" className={styles.form}>
-        <Input className={styles.input} name="email" label="이메일" type="text" placeholder='이메일'></Input>
-        <Input className={styles.input} name="password" label="비밀번호" type="password" placeholder='비밀번호'></Input>
-        <Button className={styles.button}>회원가입</Button>
+        <Input className={styles.input} forwardRef={emailRef} name="email" label="이메일" type="text" placeholder='test@test.com'></Input>
+        <Input className={styles.input} forwardRef={passwordRef} name="password" label="비밀번호" type="password" placeholder='12345678'></Input>
+        <Button className={styles.button} onClick={handleClickToRegister}>회원가입</Button>
       </Form>
     </div>
   )
